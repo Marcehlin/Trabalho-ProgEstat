fee0e50c2af1903eb4186e287b1bbc9be08a8293
Marcelo <q2650473562@gmail.com>
2025-11-18 05:43:17 -0300

    add new references

--- referencias/codigo de referencia de branching process que vem de um video no youtube.r
+++ referencias/codigo de referencia de branching process que vem de um video no youtube.r
@@ -40,3 +40,60 @@ for (i in 1:nsims) {
 }
 
 head(dados_historico[1:5, 1:10])
+
+
+set.seed(0)
+nsims <- 500
+index <- 50
+
+# function to generate paths
+FUN_branch <- function(index) {
+  population <- 1
+  for (i in 2:index) {
+    population <- c(population, sum(sample(0:2, 
+                                    population[length(population)],
+                                    replace = TRUE,
+                                    prob = c(1 / 4, 1 / 2, 1 / 4))))
+  }
+  population
+}
+
+library(data.table)
+# generate data and visualize
+data <- data.table(t = 1:index, 
+                   replicate(FUN_branch(index), n = nsims))
+data <- melt(data, id.vars = "t")
+ggplot(data, aes(x = t, y = value, col = variable)) +
+  geom_line(alpha = 0.5) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  ggtitle("Simple Branching Process") +
+  xlab("t") + ylab("size of population")
+
+
+# Sua lista de histórias
+historias <- resultados_simulacoes$critico_binom$historias
+
+# Converter a lista para data.table no formato adequado
+data <- rbindlist(
+  lapply(seq_along(historias), function(i) {
+    trajetoria <- historias[[i]]
+    # Estender a trajetória com zeros até max_length
+    trajetoria_completa <- c(trajetoria, rep(0, max_length - length(trajetoria)))
+    
+    data.table(
+      t = 1:max_length,
+      value = trajetoria_completa,
+      variable = paste0("sim", i)
+    )
+  })
+)
+
+# Criar o gráfico (código muito similar ao original)
+ggplot(data, aes(x = t, y = value, col = variable)) +
+  geom_line(alpha = 0.6) +
+  theme_bw() +
+  theme(legend.position = "none") +
+  ggtitle("Processo de Ramificação - Subcrítico Poisson") +
+  xlab("Tempo") + 
+  ylab("size of population")
